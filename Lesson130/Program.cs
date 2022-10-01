d490ae2dc827cecaa955f476f3ab1add3a8533cd
gasim <gasimmemmedov057@outlook.com>
2022-10-01 17:26:56 +0400

    HEll eledim

--- Lesson130/Program.cs
+++ Lesson130/Program.cs
@@ -7,18 +7,7 @@ namespace Lesson130
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Senin nece yasin var?");
-            string userInput = Console.ReadLine();
-            
-            var age = Int32.Parse(userInput);
-            Console.WriteLine("Senin nece yasin var?");
-            string userInput2 = Console.ReadLine();
-            int b = age - 10;
-
-            Console.WriteLine($"Senin bu qeder yaSIN VAR : {b}");
-            
-             
-             
+            Console.WriteLine("task");
         }
 
        
