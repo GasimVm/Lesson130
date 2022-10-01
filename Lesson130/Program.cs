1f7cdbeb5e7d42d50f3862825b487d4087fea683
gasim <gasimmemmedov057@outlook.com>
2022-10-01 13:48:12 +0400

    Add project files.

---
+++ Lesson130/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson130
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Senin nece yasin var?");
+            string userInput = Console.ReadLine();
+            
+            var age = Int32.Parse(userInput);
+            Console.WriteLine("Senin nece yasin var?");
+            string userInput2 = Console.ReadLine();
+            int b = age - 10;
+            Console.WriteLine($"Senin bu qeder yaSIN VAR : {b}");
+            
+             
+             
+        }
+
+       
+    }
+}
