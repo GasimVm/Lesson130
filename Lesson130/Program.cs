d16f343bc8c3d5b737e3c98073f1c9910f521327
gasim <gasimmemmedov057@outlook.com>
2022-10-01 18:33:33 +0400

    Lesson3

--- Lesson130/Program.cs
+++ Lesson130/Program.cs
@@ -6,10 +6,48 @@ namespace Lesson130
     {
         static void Main(string[] args)
         {
+            // impilicit
+            //int i = 5;
+            //double dv = i;
 
-            Console.WriteLine("task");
+            //// expilicit
+            //double dbl = 45.8;
+            //int num =(int) dbl;
+
+            //// unboxing
+            //object obj = "56";
+            //int number =(int) obj;
+            //// boxing
+            //double dbl2 = 45.7;
+            //object obj2 = dbl2;
+
+            // Random random = new Random();
+            // int ramEded = random.Next(25,50);
+            //// Console.WriteLine(ramEded);
+
+            // Guid guid = Guid.NewGuid();
+            // Console.WriteLine(guid);
+            int x = 3;
+            int y = 35;
+            string str = null;
+            // ternary operator
+            //  var rgrfr = y > x ? "" : "";
+            // var  Enboyuk = x > y ? 25.6 : 456.8;
+
+            //null coalescing
+            //   var str2 = str ?? "Null-dir";
+            // Console.WriteLine(Enboyuk);
+
+            //if ( y>x  )
+            //{
+            //    Console.WriteLine("Ela");
+            //}
+            Console.WriteLine("Main TO programcs");
+
+            
+
         }
+         
 
-       
     }
 }
