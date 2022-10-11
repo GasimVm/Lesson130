003e136149cacc4469cc4a9c331eb03202d3651a
gasim <gasimmemmedov057@outlook.com>
2022-10-11 19:12:50 +0400

    ders5

--- Lesson130/Program.cs
+++ Lesson130/Program.cs
@@ -7,9 +7,14 @@ namespace Lesson130
     {
         static void Main(string[] args)
         {
-           
-            try
-            {
+
+            // impilicit
+            //int i = 5;
+            //double dv = i;
+
+            //// expilicit
+            //double dbl = 45.8;
+            //int num =(int) dbl;
 
             //// unboxing
             //object obj = "56";
@@ -24,9 +29,9 @@ namespace Lesson130
 
             // Guid guid = Guid.NewGuid();
             // Console.WriteLine(guid);
-            int x = 3;
-            int y = 35;
-            string str = null;
+            //int x = 3;
+            //int y = 35;
+            //string str = null;
             // ternary operator
             //  var rgrfr = y > x ? "" : "";
             // var  Enboyuk = x > y ? 25.6 : 456.8;
@@ -39,12 +44,92 @@ namespace Lesson130
             //{
             //    Console.WriteLine("Ela");
             //}
-            Console.WriteLine("Main TO programcs");
+            int number = 16;
+            #region switch goto
+            //switch (number)
+            //{
+            //    case 5:
+            //        Console.WriteLine("case 5");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("case 6");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("case  7");
+            //        goto case 5;
+            //    default:
+            //        Console.WriteLine("uygun olmuyan hall");
+            //        break;
+            //}
 
-            
+            #endregion
 
+            //switch (1)
+            //{
+            //    case int x when x > 445:
+            //        Console.WriteLine("case x>15");
+            //        break;
+            //    case int x when x+5 <14:
+            //        Console.WriteLine("case x<4");
+            //        break;
+            //    default:
+            //        Console.WriteLine("uygun olmuyan hall");
+            //        break;
+            //}
+
+            //while (number>25)
+            //{
+            //    Console.WriteLine(number);
+            //    number--;
+            //}
+            int result = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i%33==0)
+                {
+                    result++;
+                    continue;
+                }
+                 
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("say"+result);
+
+            //do
+            //{
+            //    Console.WriteLine(number);
+            //    number--;
+            //} while (number > 12);
+            int b = 5;
+            b = 15;
+            //string name = "Nurlan";
+            //name += " Xankisiyev";
+            //StringBuilder stringBuilder = new StringBuilder();
+            //stringBuilder.Append("Teymur");
+            //stringBuilder.Append(" Memmed");
+            //Console.WriteLine(stringBuilder.ToString());
+        //    for (int i = 1; i < 100; i++)
+        //    {
+        //        if (i%17==0)
+        //        {
+        //            Console.WriteLine("ilk 17-e bolunewn"+i);
+        //            break;
+        //        }
+        //        Console.WriteLine(i);
+        //    }
         }
-         
+
+        static int  DoOperation(int op, int a, int b)
+        {
+            switch (op)
+            {
+                case 1: return a + b;
+                case 2: return a - b;
+                case 3: return a * b;
+                default: return 0;
+            }
+        }
+ 
 
     }
 }
